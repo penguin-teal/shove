916082dd10685c3f9af5904d96b36d76f4cec848
penguin-teal <_penguin@tuta.io>
2023-10-13 22:40:15 -0500

    Documented getTokenString

--- src/tokenUtil.c
+++ src/tokenUtil.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Gets a human readable string.
+ * This is basically the "*" part of the corresponding TOKEN_* macro.
+ * If it represents a keyword it will be all lower-case, or if it
+ * is rather a "placeholder," such as `Identifier` it will start with
+ * an upper-case letter (with the exception of `EOF`).
+ *
+ * @remarks The string must NOT be freed or managed by the caller.
+ * @return A null-terminated string identifying the given token symbol.
+ */
 char* getTokenString(int32_t token)
 {
     switch(token)
