6bc86954d2af0aa2e80dd14a8d9e0be790b714a3
penguin-teal <_penguin@tuta.io>
2023-10-13 22:09:29 -0500

    stdtypes and error messages

---
+++ include/defines.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define ERR(...) fprintf(stderr, __VA_ARGS__)
+
+#define ERR_HEAPFAIL(name) ERR("Failed to allocate heap memory for '" name "'.\n")
+
+#define NOT_IMPLEMENTED() ERR("Not Implemented")
