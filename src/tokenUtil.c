f6d328657e566f5d8ca786bfcb2562720295df4a
penguin-teal <_penguin@tuta.io>
2023-10-15 17:24:55 -0500

    Add all tokens

--- src/tokenUtil.c
+++ src/tokenUtil.c
@@ -15,6 +15,7 @@
  */
 char* getTokenString(int32_t token)
 {
+    // TODO: finish list
     switch(token)
     {
         case TOKEN_EOF:
@@ -35,11 +36,64 @@ char* getTokenString(int32_t token)
             return "Identifier";
         case TOKEN_RETURN:
             return "return";
-
+        case TOKEN_BITWISE_NOT:
+            return "~";
+        case TOKEN_BANG:
+            return "!";
+        case TOKEN_PLUS:
+            return "+";
+        case TOKEN_MINUS:
+            return "-";
+        case TOKEN_STAR:
+            return "*";
+        case TOKEN_SLASH:
+            return "/";
+        case TOKEN_MODULO:
+            return "%";
+        case TOKEN_BITWISE_AND:
+            return "&";
+        case TOKEN_BITWISE_OR:  
+            return "|";
+        case TOKEN_BITWISE_XOR:
+            return "^";
+        case TOKEN_LOGICAL_AND:
+            return "&&";
+        case TOKEN_LOGICAL_OR:
+            return "||";
+        case TOKEN_GT:
+            return ">";
+        case TOKEN_LT:
+            return "<";
+        case TOKEN_GTE:
+            return ">=";
+        case TOKEN_LTE:
+            return "<=";
+        case TOKEN_POW:
+            return "%%";
+        case TOKEN_SHOVE:
+            return "Shove";
+        case TOKEN_INTO_NAMESPACE:
+            return "::";
+        case TOKEN_HASH:
+            return "hash";
+        case TOKEN_CMP:
+            return "cmp";
+        case TOKEN_EQUAL:
+            return "=";
+        case TOKEN_NEW:
+            return "new";
+        case TOKEN_TERMINATE:
+            return ";";
+        case TOKEN_SHIFT_LEFT:
+            return "<<";
+        case TOKEN_SHIFT_RIGHT:
+            return ">>";
         case TOKEN_BLOCK_OPEN:
             return "{";
         case TOKEN_BLOCK_CLOSE:
             return "}";
+        case TOKEN_THIS:
+            return "this";
         default:
             return "Unknown";
     }
