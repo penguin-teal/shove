5043e5e85c8e40ca4f55afb13a0ee625b9221b79
penguin-teal <_penguin@tuta.io>
2023-10-19 19:53:45 -0500

    Token macros -> enum

--- include/token.h
+++ include/token.h
@@ -23,45 +23,48 @@ struct Token
     struct FilePos fpos;
 };
 
-#define TOKEN_EOF           -1  //
-#define TOKEN_NAMESPACE     -2  // namespace i
-#define TOKEN_IMPORT        -3  // import i
-#define TOKEN_EXTERN        -4  // extern i
-#define TOKEN_TABLE         -5  // table i
-#define TOKEN_INTEGER       -6  // <integer literal>
-#define TOKEN_FLOAT         -7  // <float literal>
-#define TOKEN_IDENTIFIER    -8  // <identifier>
-#define TOKEN_RETURN             -9  // return a
+enum ShvToken
+{
+    TOKEN_EOF             =   -1,  //
+    TOKEN_NAMESPACE       =   -2,  // namespace i
+    TOKEN_IMPORT          =   -3,  // import i
+    TOKEN_EXTERN          =   -4,  // extern i
+    TOKEN_TABLE           =   -5,  // table i
+    TOKEN_INTEGER         =   -6,  // <integer literal>
+    TOKEN_FLOAT           =   -7,  // <float literal>
+    TOKEN_IDENTIFIER      =   -8,  // <identifier>
+    TOKEN_RETURN          =   -9,  // return a
 
-#define TOKEN_BITWISE_NOT        -11 // ~a
-#define TOKEN_BANG               -12 // !a
-#define TOKEN_PLUS               -13 // a + b OR +a
-#define TOKEN_MINUS              -14 // a - b OR -a
-#define TOKEN_STAR               -15 // a * b
-#define TOKEN_SLASH              -16 // a / b
-#define TOKEN_MODULO             -17 // a % b
-#define TOKEN_BITWISE_AND        -18 // a & b
-#define TOKEN_BITWISE_OR         -19 // a | b
-#define TOKEN_BITWISE_XOR        -20 // a ^ b
-#define TOKEN_LOGICAL_AND        -21 // a && b
-#define TOKEN_LOGICAL_OR         -22 // a || b
-#define TOKEN_GT                 -23 // a > b
-#define TOKEN_LT                 -24 // a < b
-#define TOKEN_GTE                -25 // a >= b
-#define TOKEN_LTE                -26 // a <= b
-#define TOKEN_POW                -27 // a %% b
-#define TOKEN_SHOVE              -28 // -> or ->> or ->>> etc.
-#define TOKEN_INTO_NAMESPACE     -29 // ::
-#define TOKEN_HASH               -30 // hash a
-#define TOKEN_CMP                -31 // a cmp b
-#define TOKEN_EQUAL              -33 // i = b
-#define TOKEN_NEW                -34 // new a
-#define TOKEN_TERMINATE          -35 // a;
-#define TOKEN_SHIFT_LEFT         -36 // a << b
-#define TOKEN_SHIFT_RIGHT        -37 // a >> b
-#define TOKEN_BLOCK_OPEN         -38 // {
-#define TOKEN_BLOCK_CLOSE        -39 // }
-#define TOKEN_THIS               -40 // this
-#define TOKEN_VOID               -41 // void
-#define TOKEN_PAREN_OPEN         -42 // (
-#define TOKEN_PAREN_CLOSE        -43 // )
+    TOKEN_BITWISE_NOT     =   -11, // ~a
+    TOKEN_BANG            =   -12, // !a
+    TOKEN_PLUS            =   -13, // a + b OR +a
+    TOKEN_MINUS           =   -14, // a - b OR -a
+    TOKEN_STAR            =   -15, // a * b
+    TOKEN_SLASH           =   -16, // a / b
+    TOKEN_MODULO          =   -17, // a % b
+    TOKEN_BITWISE_AND     =   -18, // a & b
+    TOKEN_BITWISE_OR      =   -19, // a | b
+    TOKEN_BITWISE_XOR     =   -20, // a ^ b
+    TOKEN_LOGICAL_AND     =   -21, // a && b
+    TOKEN_LOGICAL_OR      =   -22, // a || b
+    TOKEN_GT              =   -23, // a > b
+    TOKEN_LT              =   -24, // a < b
+    TOKEN_GTE             =   -25, // a >= b
+    TOKEN_LTE             =   -26, // a <= b
+    TOKEN_POW             =   -27, // a %% b
+    TOKEN_SHOVE           =   -28, // -> or ->> or ->>> etc.
+    TOKEN_INTO_NAMESPACE  =   -29, // ::
+    TOKEN_HASH            =   -30, // hash a
+    TOKEN_CMP             =   -31, // a cmp b
+    TOKEN_EQUAL           =   -33, // i = b
+    TOKEN_NEW             =   -34, // new a
+    TOKEN_TERMINATE       =   -35, // a;
+    TOKEN_SHIFT_LEFT      =   -36, // a << b
+    TOKEN_SHIFT_RIGHT     =   -37, // a >> b
+    TOKEN_BLOCK_OPEN      =   -38, // {
+    TOKEN_BLOCK_CLOSE     =   -39, // }
+    TOKEN_THIS            =   -40, // this
+    TOKEN_VOID            =   -41, // void
+    TOKEN_PAREN_OPEN      =   -42, // (
+    TOKEN_PAREN_CLOSE     =   -43, // )
+};
