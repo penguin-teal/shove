5825c307a390efc30e9df6fd98584e7d17264dc2
penguin-teal <_penguin@tuta.io>
2023-10-23 14:42:27 -0500

    Use options and search directory for src files

--- src/main.c
+++ src/main.c
@@ -1,71 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 #include "defines.h"
 #include "fileIo.h"
 #include "lexer.h"
 #include "ir.h"
 #include "shvError.h"
 #include "appArgs.h"
+#include "strOp.h"
 
 int main(int argc, char **argv)
 {
-    int ret = 0;
-
     struct AppArgs appArgs = { 0 };
     if(!doArgp(&appArgs, argc, argv)) return 2;
 
-    // Right now we're just gonna open this test
-    FILE *f = fopen("./tests/add/main.shv", "r");
-    // If file couldn't be opened
-    if(!f)
+    if(appArgs.pedantic) setPedanticShvIssueSeverities();
+    else if(appArgs.streetRules) setStreetRulesShvIssueSeverities();
+    else setDefaultShvIssueSeverities();
+
+    if(appArgs.warn) setShvIssueString(appArgs.warn, SEVERITY_WARNING);
+    if(appArgs.err) setShvIssueString(appArgs.err, SEVERITY_ERROR);
+
+    char *dirName = appArgs.args[0];
+    size_t dirNameLen = strlen(dirName);
+    DIR *srcDir = opendir(dirName);
+    if(!srcDir)
     {
-        ERR("Failed to open source file.\n");
-        // 2 indicates user error --- we got fed an invalid path
+        ERR("Failed to open source directory '%s'.\n", dirName);
         return 2;
     }
-    else
+
+    struct dirent *dirEntry;
+
+    size_t fullPathSize = 1024;
+    char *fullPath = malloc(fullPathSize);
+    if(!fullPath)
     {
-        char fileName[] = "main.shv";
-        // Get file size
-        // lexFile uses this as a basis for deciding the initial capacity
-        // of vectors
-        size_t size = fGetSize(f);
+        ERR("%lu B malloc failed.\n", fullPathSize);
+        closedir(srcDir);
+        return 1;
+    }
+
+    while((dirEntry = readdir(srcDir)))
+    {
+        char *fName = dirEntry->d_name;
+        size_t fNameLen = strlen(fName);
+
+        if(*fName == '.' && ((fName[1] == '.' && fName[2] == 0) || fName[1] == 0)) continue;
+
+        size_t fullPathLen = dirNameLen + fNameLen + 2;
+        if(fullPathSize < fullPathLen)
+        {
+            fullPathSize = fullPathLen;
+            fullPath = realloc(fullPath, fullPathSize);
+            if(!fullPath)
+            {
+                ERR("%lu B realloc failed.", fullPathSize);
+                free(fullPath);
+                closedir(srcDir);
+                return 1;
+            }
+        }
+
+        memcpy(fullPath, dirName, dirNameLen);
+        if(dirName[dirNameLen - 1] != '/' && *fName != '/')
+        {
+            fullPath[dirNameLen] = '/';
+            memcpy(fullPath + dirNameLen + 1, fName, fNameLen + 1);
+        }
+        else memcpy(fullPath + dirNameLen, fName, fNameLen + 1);
+
+        if(!strEndsWith(fName, ".shv"))
+        {
+            if(appArgs.verbose)
+            {
+                printf("Skipping file '%s' because it doesn't end with '.shv'.\n", fullPath);
+            }
+            continue;
+        }
+
+        FILE *f = fopen(fullPath, "r");
+        if(!f)
+        {
+            ERR("Failed to open source file '%s'.\n", fullPath);
+            free(fullPath);
+            closedir(srcDir);
+            return 1;
+        }
+
+        if(appArgs.verbose)
+        {
+            printf("Compiling shove source file '%s'.\n", fullPath);
+        }
+
+        size_t fSize = fGetSize(f);
 
         struct Token *tokens;
         char *strings;
 
-        setDefaultShvIssueSeverities();
-
-        if(!lexFile(f, size, fileName, &tokens, &strings))
+        if(!lexFile(f, fSize, fName, &tokens, &strings))
         {
             // We don't have to free tokens or strings on fail
-            fprintf(
-                stderr,
+            ERR(
                 "Compilation Failed: Lexing Failed\n"
             );
-            ret = 1;
-            goto Cleanup;
+            free(fullPath);
+            fclose(f);
+            closedir(srcDir);
+            return 1;
         }
 
         if(!compileLlvm(tokens, strings))
         {
             // We have to free tokens and strings when this fails
             // since lexFile succeeded
-            fprintf(
-                stderr,
+            ERR(
                 "Compilation Failed: Compiling Failed\n"
             );
-            ret = 1;
+            free(fullPath);
             free(tokens);
             free(strings);
-            goto Cleanup;
+            fclose(f);
+            closedir(srcDir);
+            return 1;
         }
 
         free(tokens);
         free(strings);
+
+        fclose(f);
     }
 
-Cleanup:
-    fclose(f);
-    return ret;
+    free(fullPath);
+    closedir(srcDir);
+    return 0;
 }
